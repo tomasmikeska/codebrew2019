581c1c7f20490e2118dc34850334951cf4fdefe4
Tomáš Mikeska <tomikeska@gmail.com>
2019-11-09 23:15:34 +0100

    add face identification

--- face-recognition/src/utils.py
+++ face-recognition/src/utils.py
@@ -1,4 +1,15 @@
-from os.path import join, dirname
+from os import listdir
+from os.path import isfile, isdir, join, exists, dirname
+
+
+def file_listing(dir):
+    '''
+    List all files (exclude dirs) in specified directory
+
+    Args:
+        dir (string): Director full path
+    '''
+    return [f for f in listdir(dir) if isfile(join(dir, f))]
 
 
 def relative_path(path):
