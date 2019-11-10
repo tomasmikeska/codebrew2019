0389aeb83e775b8cf0297cafaf9e9565548db16d
Jakub Kriz <jakub.kriz@artin.cz>
2019-11-10 10:46:44 +0100

    add current time to weather integration

--- backend/src/intentExtendActions.ts
+++ backend/src/intentExtendActions.ts
@@ -1,6 +1,9 @@
+const moment = require('moment');
+
 const call = require('./integrations/call'); // call.callTaxi('+420774532168', `Hi, we would like to order taxi service from our office to ${'taxi_destination:'} at ${time}`);
 const sms = require('./integrations/sms');  // sms.sendSms('+421918068460', 'kokot');
 const mail = require('./integrations/calendar'); //mail.sendMailWithCalendarEntry()
+const weather = require('./integrations/weather');
 
 
 
@@ -8,13 +11,16 @@ module.exports = {
   async processIntent(intentName: any, intent: any, socket: any) {
     console.log('ExtendAction: ', intentName);
 
-    switch(intentName) {
+    switch (intentName) {
       case "taxi":
         call.callTaxi('+420774532168', `Hi, we would like to order taxi service from our office to ${intent.context.taxi_destination} at ${intent.context.time}`);
         break;
       case "reserve-meeting":
         sms.sendSms('+421949674752', `reserved meeting room: ${intent.context.room}`);
         mail.sendMailWithCalendarEntry();
+        break;
+      case "weather":
+        console.log(weather.getWeatherConditions(intent.context.date, moment().format('hh:mm:ss'), intent.context.location))
         break;
       default:
         return;
