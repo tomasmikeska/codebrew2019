b682ed684f2d67f0b70b8b0f6742706e60770d09
Jakub Kriz <jakub.kriz@artin.cz>
2019-11-10 08:01:36 +0100

    add weather api example

---
+++ backend/src/integrations/weather.ts
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+module.exports = {
+    async getWeatherConditions() {
+        try {
+            const date_txt = '2019-11-10'
+            const time_txt = '19:31:01'
+            const city = 'Brno'
+            const token = '27137b0fa76c7ef4daa3da3e130cf98f'
+            const rslt = (await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city},cz&units=metric&mode=json&APPID=${token}`)).data;
+            console.log(
+                rslt.list.reduce((rsltArr: any, weatherData: any) => {
+                    return this.isClosestWeatherData(weatherData, date_txt, time_txt, 3) ? rsltArr.concat({
+                        temp: weatherData.main.temp,
+                        desc: weatherData.weather[0].description
+                    }) : rsltArr
+                }, []))
+        } catch (exc) {
+            console.error(exc)
+        }
+    },
+
+    isClosestWeatherData(weatherData: any, date_txt: String, time_txt: String, nHoursModel: number) {
+        const hour = parseInt(time_txt.split(':')[0])
+        const hourReminder = hour % nHoursModel
+        const hoursDiff = Math.floor(nHoursModel / 2)
+        const rsltHours = hourReminder <= hoursDiff ? hour - hoursDiff : hour + hoursDiff;
+        return weatherData.dt_txt === `${date_txt} ${rsltHours}:00:00`
+    }
+}
